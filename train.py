c59054acb65475308132626cf2dde6d42dcc45d3
osmium <1711042219@qq.com>
2020-02-28 22:35:58 +0800

    fixed some bugs

--- train.py
+++ train.py
@@ -168,7 +168,7 @@ with tf.Session() as sess:
 	kappa = calKappa(probMap.map, allLabeledLabel)
 	mixMatrix = calMixMatrix(probMap.map, allLabeledLabel)
 
-	with open(os.path.join(dataSavePath, "summary"), "w+") as f:
+	with open(os.path.join(dataSavePath, "summary.txt"), "w+") as f:
 		print("OA: %4f, AA: %4f, KAPPA: %4f" % (OA, AA, kappa), file=f)
 		print("******* MIX MAP *******", file=f)
 		print("   |", file=f)
