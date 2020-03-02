1018aa773b42fae73489ec7cc4e73e1e9824707b
icpc <1711042219@qq.com>
2020-03-02 13:17:48 +0800

    added win support

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
-python ./train.py --data 6 -e 50 -r 0.1 -a 2 -l 0.0001 -d ./save/data/feb28/KSC --model_path ./save/model/feb28/KSC -g 1
-python ./train.py --data 1 -e 50 -r 0.1 -a 2 -l 0.0001 -d ./save/data/feb28/idp --model_path ./save/model/feb28/idp -g 1
-python ./train.py --data 2 -e 50 -r 0.1 -a 2 -l 0.0001 -d ./save/data/feb28/pu --model_path ./save/model/feb28/pu -g 1
-python ./train.py --data 3 -e 50 -r 0.1 -a 2 -l 0.0001 -d ./save/data/feb28/pc --model_path ./save/model/feb28/pc -g 1
-python ./train.py --data 4 -e 50 -r 0.1 -a 2 -l 0.0001 -d ./save/data/feb28/salinas --model_path ./save/model/feb28/salinas -g 1
-python ./train.py --data 5 -e 50 -r 0.1 -a 2 -l 0.0001 -d ./save/data/feb28/SA --model_path ./save/model/feb28/SA -g 1
+python ./train.py --data 1 -e 50 -r 0.1 -a 2 -l 0.0001 -d ./save/data/mar2/idp --model_path ./save/model/mar2/idp -g 1
+python ./train.py --data 2 -e 50 -r 0.1 -a 2 -l 0.0001 -d ./save/data/mar2/pu --model_path ./save/model/mar2/pu -g 1
+python ./train.py --data 3 -e 50 -r 0.1 -a 2 -l 0.0001 -d ./save/data/mar2/pc --model_path ./save/model/mar2/pc -g 1
+python ./train.py --data 4 -e 50 -r 0.1 -a 2 -l 0.0001 -d ./save/data/mar2/salinas --model_path ./save/model/mar2/salinas -g 1
+python ./train.py --data 5 -e 50 -r 0.1 -a 2 -l 0.0001 -d ./save/data/mar2/SA --model_path ./save/model/mar2/SA -g 1
+python ./train.py --data 6 -e 50 -r 0.1 -a 2 -l 0.0001 -d ./save/data/mar2/KSC --model_path ./save/model/mar2/KSC -g 1
