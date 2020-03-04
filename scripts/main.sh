758f414490f840f66435e69aed4cd090464fc6c8
osmium <1711042219@qq.com>
2020-03-04 12:43:12 +0800

    add test scripts

---
+++ scripts/main.sh
@@ -0,0 +1,1 @@
+python ../train.py -e 50 -b 50 -l 0.0001 -g 0 -r 0.1 -a 1 -p 7 -m 1 -d ./save/mar4-0/ --model_path ./save/mar4-0/ --data 2 --first_layer 6 --second_layer 8 --dont_save_data --no_detailed_summary
