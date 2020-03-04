d2cec67a575f8fbb4586b0a28ba8a4ff60b760f7
osmium <1711042219@qq.com>
2020-03-04 13:45:16 +0800

    add test scripts

--- scripts/genScripts.py
+++ scripts/genScripts.py
@@ -1,3 +1,13 @@
 if __name__ == '__main__':
-	with open ("main.sh","w+") as f:
-		print("python ../train.py -e 50 -b 50 -l 0.0001 -g 0 -r 0.1 -a 1 -p 7 -m 1 -d ./save/mar4-0/ --model_path ./save/mar4-0/ --data 2 --first_layer 6 --second_layer 8 --dont_save_data --no_detailed_summary",file=f)
+	with open("main.sh", "w+") as f:
+		for l in (0.0001,0.0005):
+			for a in (5,):
+				for p in (7,9):
+					for e in (50,):
+						for data in (2,4,5,6):
+							for first_layer in (6,8,10,12):
+								for second_layer in (6,8,10,12,14,16):
+									print(
+										"python ../train.py -e %d -b 50 -l %.4f -g 0 -r 0.1 -a %d -p %d -m 1 -d ./save/mar4-0/ --model_path ./save/mar4-0/ --data %d --first_layer %d --second_layer %d --dont_save_data --no_detailed_summary"\
+										%(e,l,a,p,data,first_layer,second_layer),
+										end="\n", file=f)
