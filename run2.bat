b926dd581a2466629eafdf6ecb5d2f1eb453b9ec
osmium <1711042219@qq.com>
2020-03-07 11:27:34 +0800

    added exp bat

--- run2.bat
+++ run2.bat
@@ -1,31 +1,31 @@
-python ./train.py -g 1 -e 100 --data 2 -l 0.0001 -p 5 -r 0.1 -m 2 -d .\save\formal0307\data\CapsNet\pu\1\ --model_path .\save\formal0307\data\CapsNet\pu\1\ --first_layer 8 --second_layer 16 --use_best_model --draw
-python ./train.py -g 1 -e 100 --data 2 -l 0.0001 -p 5 -r 0.1 -m 2 -d .\save\formal0307\data\CapsNet\pu\2\ --model_path .\save\formal0307\data\CapsNet\pu\2\ --first_layer 8 --second_layer 16 --use_best_model --dont_save_data
-python ./train.py -g 1 -e 100 --data 2 -l 0.0001 -p 5 -r 0.1 -m 2 -d .\save\formal0307\data\CapsNet\pu\3\ --model_path .\save\formal0307\data\CapsNet\pu\3\ --first_layer 8 --second_layer 16 --use_best_model --dont_save_data
-python ./train.py -g 1 -e 100 --data 2 -l 0.0001 -p 5 -r 0.1 -m 2 -d .\save\formal0307\data\CapsNet\pu\4\ --model_path .\save\formal0307\data\CapsNet\pu\4\ --first_layer 8 --second_layer 16 --use_best_model --dont_save_data
-python ./train.py -g 1 -e 100 --data 2 -l 0.0001 -p 5 -r 0.1 -m 2 -d .\save\formal0307\data\CapsNet\pu\5\ --model_path .\save\formal0307\data\CapsNet\pu\5\ --first_layer 8 --second_layer 16 --use_best_model --dont_save_data
+python ./train.py -g 1 -e 100 --data 2 -l 0.0001 -p 5 -r 0.1 -m 2 -d .\save\formal0307\data\CapsNet\pu\1\ --model_path .\save\formal0307\model\CapsNet\pu\1\ --first_layer 8 --second_layer 16 --use_best_model --draw
+python ./train.py -g 1 -e 100 --data 2 -l 0.0001 -p 5 -r 0.1 -m 2 -d .\save\formal0307\data\CapsNet\pu\2\ --model_path .\save\formal0307\model\CapsNet\pu\2\ --first_layer 8 --second_layer 16 --use_best_model --dont_save_data
+python ./train.py -g 1 -e 100 --data 2 -l 0.0001 -p 5 -r 0.1 -m 2 -d .\save\formal0307\data\CapsNet\pu\3\ --model_path .\save\formal0307\model\CapsNet\pu\3\ --first_layer 8 --second_layer 16 --use_best_model --dont_save_data
+python ./train.py -g 1 -e 100 --data 2 -l 0.0001 -p 5 -r 0.1 -m 2 -d .\save\formal0307\data\CapsNet\pu\4\ --model_path .\save\formal0307\model\CapsNet\pu\4\ --first_layer 8 --second_layer 16 --use_best_model --dont_save_data
+python ./train.py -g 1 -e 100 --data 2 -l 0.0001 -p 5 -r 0.1 -m 2 -d .\save\formal0307\data\CapsNet\pu\5\ --model_path .\save\formal0307\model\CapsNet\pu\5\ --first_layer 8 --second_layer 16 --use_best_model --dont_save_data
 
-python ./train.py -g 1 -e 100 --data 3 -l 0.0001 -p 5 -r 0.1 -m 2 -d .\save\formal0307\data\CapsNet\pc\1\ --model_path .\save\formal0307\data\CapsNet\pc\1\ --first_layer 8 --second_layer 16 --use_best_model --draw
-python ./train.py -g 1 -e 100 --data 3 -l 0.0001 -p 5 -r 0.1 -m 2 -d .\save\formal0307\data\CapsNet\pc\2\ --model_path .\save\formal0307\data\CapsNet\pc\2\ --first_layer 8 --second_layer 16 --use_best_model --dont_save_data
-python ./train.py -g 1 -e 100 --data 3 -l 0.0001 -p 5 -r 0.1 -m 2 -d .\save\formal0307\data\CapsNet\pc\3\ --model_path .\save\formal0307\data\CapsNet\pc\3\ --first_layer 8 --second_layer 16 --use_best_model --dont_save_data
-python ./train.py -g 1 -e 100 --data 3 -l 0.0001 -p 5 -r 0.1 -m 2 -d .\save\formal0307\data\CapsNet\pc\4\ --model_path .\save\formal0307\data\CapsNet\pc\4\ --first_layer 8 --second_layer 16 --use_best_model --dont_save_data
-python ./train.py -g 1 -e 100 --data 3 -l 0.0001 -p 5 -r 0.1 -m 2 -d .\save\formal0307\data\CapsNet\pc\5\ --model_path .\save\formal0307\data\CapsNet\pc\5\ --first_layer 8 --second_layer 16 --use_best_model --dont_save_data
+python ./train.py -g 1 -e 100 --data 3 -l 0.0001 -p 5 -r 0.1 -m 2 -d .\save\formal0307\data\CapsNet\pc\1\ --model_path .\save\formal0307\model\CapsNet\pc\1\ --first_layer 8 --second_layer 16 --use_best_model --draw
+python ./train.py -g 1 -e 100 --data 3 -l 0.0001 -p 5 -r 0.1 -m 2 -d .\save\formal0307\data\CapsNet\pc\2\ --model_path .\save\formal0307\model\CapsNet\pc\2\ --first_layer 8 --second_layer 16 --use_best_model --dont_save_data
+python ./train.py -g 1 -e 100 --data 3 -l 0.0001 -p 5 -r 0.1 -m 2 -d .\save\formal0307\data\CapsNet\pc\3\ --model_path .\save\formal0307\model\CapsNet\pc\3\ --first_layer 8 --second_layer 16 --use_best_model --dont_save_data
+python ./train.py -g 1 -e 100 --data 3 -l 0.0001 -p 5 -r 0.1 -m 2 -d .\save\formal0307\data\CapsNet\pc\4\ --model_path .\save\formal0307\model\CapsNet\pc\4\ --first_layer 8 --second_layer 16 --use_best_model --dont_save_data
+python ./train.py -g 1 -e 100 --data 3 -l 0.0001 -p 5 -r 0.1 -m 2 -d .\save\formal0307\data\CapsNet\pc\5\ --model_path .\save\formal0307\model\CapsNet\pc\5\ --first_layer 8 --second_layer 16 --use_best_model --dont_save_data
 
-python ./train.py -g 1 -e 100 --data 4 -l 0.0001 -p 5 -r 0.1 -m 2 -d .\save\formal0307\data\CapsNet\sl\1\ --model_path .\save\formal0307\data\CapsNet\sl\1\ --first_layer 8 --second_layer 16 --use_best_model --draw
-python ./train.py -g 1 -e 100 --data 4 -l 0.0001 -p 5 -r 0.1 -m 2 -d .\save\formal0307\data\CapsNet\sl\2\ --model_path .\save\formal0307\data\CapsNet\sl\2\ --first_layer 8 --second_layer 16 --use_best_model --dont_save_data
-python ./train.py -g 1 -e 100 --data 4 -l 0.0001 -p 5 -r 0.1 -m 2 -d .\save\formal0307\data\CapsNet\sl\3\ --model_path .\save\formal0307\data\CapsNet\sl\3\ --first_layer 8 --second_layer 16 --use_best_model --dont_save_data
-python ./train.py -g 1 -e 100 --data 4 -l 0.0001 -p 5 -r 0.1 -m 2 -d .\save\formal0307\data\CapsNet\sl\4\ --model_path .\save\formal0307\data\CapsNet\sl\4\ --first_layer 8 --second_layer 16 --use_best_model --dont_save_data
-python ./train.py -g 1 -e 100 --data 4 -l 0.0001 -p 5 -r 0.1 -m 2 -d .\save\formal0307\data\CapsNet\sl\5\ --model_path .\save\formal0307\data\CapsNet\sl\5\ --first_layer 8 --second_layer 16 --use_best_model --dont_save_data
+python ./train.py -g 1 -e 100 --data 4 -l 0.0001 -p 5 -r 0.1 -m 2 -d .\save\formal0307\data\CapsNet\sl\1\ --model_path .\save\formal0307\model\CapsNet\sl\1\ --first_layer 8 --second_layer 16 --use_best_model --draw
+python ./train.py -g 1 -e 100 --data 4 -l 0.0001 -p 5 -r 0.1 -m 2 -d .\save\formal0307\data\CapsNet\sl\2\ --model_path .\save\formal0307\model\CapsNet\sl\2\ --first_layer 8 --second_layer 16 --use_best_model --dont_save_data
+python ./train.py -g 1 -e 100 --data 4 -l 0.0001 -p 5 -r 0.1 -m 2 -d .\save\formal0307\data\CapsNet\sl\3\ --model_path .\save\formal0307\model\CapsNet\sl\3\ --first_layer 8 --second_layer 16 --use_best_model --dont_save_data
+python ./train.py -g 1 -e 100 --data 4 -l 0.0001 -p 5 -r 0.1 -m 2 -d .\save\formal0307\data\CapsNet\sl\4\ --model_path .\save\formal0307\model\CapsNet\sl\4\ --first_layer 8 --second_layer 16 --use_best_model --dont_save_data
+python ./train.py -g 1 -e 100 --data 4 -l 0.0001 -p 5 -r 0.1 -m 2 -d .\save\formal0307\data\CapsNet\sl\5\ --model_path .\save\formal0307\model\CapsNet\sl\5\ --first_layer 8 --second_layer 16 --use_best_model --dont_save_data
 
 
-python ./train.py -g 1 -e 100 --data 5 -l 0.0001 -p 5 -r 0.1 -m 2 -d .\save\formal0307\data\CapsNet\sa\1\ --model_path .\save\formal0307\data\CapsNet\sa\1\ --first_layer 8 --second_layer 16 --use_best_model --draw
-python ./train.py -g 1 -e 100 --data 5 -l 0.0001 -p 5 -r 0.1 -m 2 -d .\save\formal0307\data\CapsNet\sa\2\ --model_path .\save\formal0307\data\CapsNet\sa\2\ --first_layer 8 --second_layer 16 --use_best_model --dont_save_data
-python ./train.py -g 1 -e 100 --data 5 -l 0.0001 -p 5 -r 0.1 -m 2 -d .\save\formal0307\data\CapsNet\sa\3\ --model_path .\save\formal0307\data\CapsNet\sa\3\ --first_layer 8 --second_layer 16 --use_best_model --dont_save_data
-python ./train.py -g 1 -e 100 --data 5 -l 0.0001 -p 5 -r 0.1 -m 2 -d .\save\formal0307\data\CapsNet\sa\4\ --model_path .\save\formal0307\data\CapsNet\sa\4\ --first_layer 8 --second_layer 16 --use_best_model --dont_save_data
-python ./train.py -g 1 -e 100 --data 5 -l 0.0001 -p 5 -r 0.1 -m 2 -d .\save\formal0307\data\CapsNet\sa\5\ --model_path .\save\formal0307\data\CapsNet\sa\5\ --first_layer 8 --second_layer 16 --use_best_model --dont_save_data
+python ./train.py -g 1 -e 100 --data 5 -l 0.0001 -p 5 -r 0.1 -m 2 -d .\save\formal0307\data\CapsNet\sa\1\ --model_path .\save\formal0307\model\CapsNet\sa\1\ --first_layer 8 --second_layer 16 --use_best_model --draw
+python ./train.py -g 1 -e 100 --data 5 -l 0.0001 -p 5 -r 0.1 -m 2 -d .\save\formal0307\data\CapsNet\sa\2\ --model_path .\save\formal0307\model\CapsNet\sa\2\ --first_layer 8 --second_layer 16 --use_best_model --dont_save_data
+python ./train.py -g 1 -e 100 --data 5 -l 0.0001 -p 5 -r 0.1 -m 2 -d .\save\formal0307\data\CapsNet\sa\3\ --model_path .\save\formal0307\model\CapsNet\sa\3\ --first_layer 8 --second_layer 16 --use_best_model --dont_save_data
+python ./train.py -g 1 -e 100 --data 5 -l 0.0001 -p 5 -r 0.1 -m 2 -d .\save\formal0307\data\CapsNet\sa\4\ --model_path .\save\formal0307\model\CapsNet\sa\4\ --first_layer 8 --second_layer 16 --use_best_model --dont_save_data
+python ./train.py -g 1 -e 100 --data 5 -l 0.0001 -p 5 -r 0.1 -m 2 -d .\save\formal0307\data\CapsNet\sa\5\ --model_path .\save\formal0307\model\CapsNet\sa\5\ --first_layer 8 --second_layer 16 --use_best_model --dont_save_data
 
-python ./train.py -g 1 -e 100 --data 6 -l 0.0001 -p 5 -r 0.1 -m 2 -d .\save\formal0307\data\CapsNet\ksc\1\ --model_path .\save\formal0307\data\CapsNet\ksc\1\ --first_layer 8 --second_layer 16 --use_best_model --draw
-python ./train.py -g 1 -e 100 --data 6 -l 0.0001 -p 5 -r 0.1 -m 2 -d .\save\formal0307\data\CapsNet\ksc\2\ --model_path .\save\formal0307\data\CapsNet\ksc\2\ --first_layer 8 --second_layer 16 --use_best_model --dont_save_data
-python ./train.py -g 1 -e 100 --data 6 -l 0.0001 -p 5 -r 0.1 -m 2 -d .\save\formal0307\data\CapsNet\ksc\3\ --model_path .\save\formal0307\data\CapsNet\ksc\3\ --first_layer 8 --second_layer 16 --use_best_model --dont_save_data
-python ./train.py -g 1 -e 100 --data 6 -l 0.0001 -p 5 -r 0.1 -m 2 -d .\save\formal0307\data\CapsNet\ksc\4\ --model_path .\save\formal0307\data\CapsNet\ksc\4\ --first_layer 8 --second_layer 16 --use_best_model --dont_save_data
-python ./train.py -g 1 -e 100 --data 6 -l 0.0001 -p 5 -r 0.1 -m 2 -d .\save\formal0307\data\CapsNet\ksc\5\ --model_path .\save\formal0307\data\CapsNet\ksc\5\ --first_layer 8 --second_layer 16 --use_best_model --dont_save_data
+python ./train.py -g 1 -e 100 --data 6 -l 0.0001 -p 5 -r 0.1 -m 2 -d .\save\formal0307\data\CapsNet\ksc\1\ --model_path .\save\formal0307\model\CapsNet\ksc\1\ --first_layer 8 --second_layer 16 --use_best_model --draw
+python ./train.py -g 1 -e 100 --data 6 -l 0.0001 -p 5 -r 0.1 -m 2 -d .\save\formal0307\data\CapsNet\ksc\2\ --model_path .\save\formal0307\model\CapsNet\ksc\2\ --first_layer 8 --second_layer 16 --use_best_model --dont_save_data
+python ./train.py -g 1 -e 100 --data 6 -l 0.0001 -p 5 -r 0.1 -m 2 -d .\save\formal0307\data\CapsNet\ksc\3\ --model_path .\save\formal0307\model\CapsNet\ksc\3\ --first_layer 8 --second_layer 16 --use_best_model --dont_save_data
+python ./train.py -g 1 -e 100 --data 6 -l 0.0001 -p 5 -r 0.1 -m 2 -d .\save\formal0307\data\CapsNet\ksc\4\ --model_path .\save\formal0307\model\CapsNet\ksc\4\ --first_layer 8 --second_layer 16 --use_best_model --dont_save_data
+python ./train.py -g 1 -e 100 --data 6 -l 0.0001 -p 5 -r 0.1 -m 2 -d .\save\formal0307\data\CapsNet\ksc\5\ --model_path .\save\formal0307\model\CapsNet\ksc\5\ --first_layer 8 --second_layer 16 --use_best_model --dont_save_data
  
